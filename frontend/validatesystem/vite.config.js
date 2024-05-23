0dac16578a2a3ba49d97145273c44e7139cd2a91
saqib mushtaq <irfanrather4627@gmail.com>
2024-05-23 15:24:15 +0530

    ReadyForDeployment

--- frontend/validatesystem/vite.config.js
+++ frontend/validatesystem/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const port = process.env.PORT || 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
-})
+  server: {
+    port,
+  },
+});
